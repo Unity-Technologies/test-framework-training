2dad9e59d654a54c9db0e4eaee1e16ee90f7f94c
Christian Warnecke <christianw@hq.unity3d.com>
2019-01-28 10:30:11 +0100

    Initial ball drop exercise.

---
+++ Assets/BallDrop/BallControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallControl : MonoBehaviour
+{
+    public Vector3 Force;
+    public float SecondsForceApplied;
+    public Material[] Materials;
+
+    int materialIndex = -1;
+    float startTime;
+
+    // Use this for initialization
+    void Start ()
+    {
+        startTime = Time.fixedTime;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Time.fixedTime < startTime + SecondsForceApplied)
+        {
+            var rigidBody = GetComponent<Rigidbody>();
+            rigidBody.AddForceAtPosition(Force * 1000 * Time.deltaTime, transform.position);
+        }
+    }
+
+    public void SwapMaterial()
+    {
+        materialIndex++;
+        if (materialIndex >= Materials.Length)
+        {
+            materialIndex = 0;
+        }
+
+        var renderer = GetComponent<Renderer>();
+        renderer.material = Materials[materialIndex];
+    }
+
+    public void SetBallColor(Color color)
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.sharedMaterial.color = color;
+    }
+
+    public Color GetBallColor()
+    {
+        var renderer = GetComponent<Renderer>();
+        return renderer.sharedMaterial.color;
+    }
+}
---
+++ Assets/Editor/BallControlEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BallControl))]
+public class BallControlEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var control = (BallControl)target;
+        if (GUILayout.Button("Swap Material"))
+        {
+            control.SwapMaterial();
+        }
+    }
+}
