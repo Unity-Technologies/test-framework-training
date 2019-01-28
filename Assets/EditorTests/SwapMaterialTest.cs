6a54e34dcd9ecdb5d710acde7bcba7e0318eb4c0
Christian Warnecke <christianw@hq.unity3d.com>
2019-01-28 14:24:58 +0100

    Added test for swap materials error case.

--- Assets/EditorTests/SwapMaterialTest.cs
+++ Assets/EditorTests/SwapMaterialTest.cs
@@ -1,33 +1,47 @@
 ﻿using UnityEngine;
 using NUnit.Framework;
 using UnityEditor.SceneManagement;
+using UnityEngine.TestTools;
 
 public class SwapMaterialTest
 {
+    private BallControl m_ballControl;
+    private Renderer m_renderer;
 
-    [Test]
-    public void BallControl_SwapMaterialTests()
+    [SetUp]
+    public void Setup()
     {
         EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
         var gameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         gameObject.AddComponent<Rigidbody>();
-        var ballControl = gameObject.AddComponent<BallControl>();
-        var renderer = gameObject.GetComponent<Renderer>();
-
+        m_ballControl = gameObject.AddComponent<BallControl>();
+        m_renderer = gameObject.GetComponent<Renderer>();
+    }
+    
+    [Test]
+    public void BallControl_SwapMaterialTests()
+    {
         var material1 = new Material(Shader.Find("Specular"));
         material1.color = Color.red;
         var material2 = new Material(Shader.Find("Specular"));
         material2.color = Color.blue;
 
-        ballControl.Materials = new[] { material1, material2 };
+        m_ballControl.Materials = new[] { material1, material2 };
 
-        ballControl.SwapMaterial();
-        Assert.AreEqual(material1, renderer.sharedMaterial);
+        m_ballControl.SwapMaterial();
+        Assert.AreEqual(material1, m_renderer.sharedMaterial);
 
-        ballControl.SwapMaterial();
-        Assert.AreEqual(material2, renderer.sharedMaterial);
+        m_ballControl.SwapMaterial();
+        Assert.AreEqual(material2, m_renderer.sharedMaterial);
 
-        ballControl.SwapMaterial();
-        Assert.AreEqual(material1, renderer.sharedMaterial);
+        m_ballControl.SwapMaterial();
+        Assert.AreEqual(material1, m_renderer.sharedMaterial);
+    }
+
+    [Test]
+    public void MallControl_SwapMaterialsError()
+    {
+        LogAssert.Expect(LogType.Error, "No materials available for swapping.");
+        m_ballControl.SwapMaterial();
     }
 }
